8b3a088e9fd751004e17a99e08f1dc03e6086522
Miqdad Yanuar Farcha <miqdad.farcha@gmail.com>
2020-07-05 20:12:51 +0700

    Fix Quote

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const GIT_USERNAME = process.env.GIT_USERNAME;
 const GIT_TOKEN = process.env.GIT_TOKEN;
 const GIT_SOURCE = process.env.GIT_SOURCE;
 
-exec('echo https://${GIT_USERNAME}:${GIT_TOKEN}@${GIT_SOURCE} > ~/.git-credentials
+exec(`echo https://${GIT_USERNAME}:${GIT_TOKEN}@${GIT_SOURCE} > ~/.git-credentials
      && cd /repository
      && git config credential.helper store
      && git config --global user.email "git-puller@github.com"
-     && git config --global user.name "Github Puller" ')
+     && git config --global user.name "Github Puller"`)
        .then(() => console.log("credential updated"))
 
 http.createServer(function (req, res) {

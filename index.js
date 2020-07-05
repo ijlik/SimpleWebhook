b568d0fadaa8edb639e9cc5b802ab64459f8d998
Miqdad Yanuar Farcha <miqdad.farcha@gmail.com>
2020-07-05 20:49:17 +0700

    Tidy Up Code and Console

--- index.js
+++ index.js
@@ -9,32 +9,43 @@ const GIT_USERNAME = process.env.GIT_USERNAME;
 const GIT_TOKEN = process.env.GIT_TOKEN;
 const GIT_SOURCE = process.env.GIT_SOURCE;
 
-exec(`echo https://${GIT_USERNAME}:${GIT_TOKEN}@${GIT_SOURCE} > ~/.git-credentials
-     && cd /repository
-     && git config credential.helper store
-     && git config --global user.email "git-puller@github.com"
-     && git config --global user.name "Github Puller"`)
-       .then(() => console.log("credential updated"))
+const commands = [
+	`echo https://${GIT_USERNAME}:${GIT_TOKEN}@${GIT_SOURCE} > ~/.git-credential`,
+	`cd /repository`,
+	`git config --global credential.helper store`,
+	`git config --global user.email "git-puller@github.com"`,
+	`git config --global user.name "Github Puller"`
+];
+
+commands.forEach((command) => {
+	exec(command)
+		.then(() => {
+			console.log(`Run Command : ${command}`);
+		});
+});
 
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
     res.statusCode = 404
-    res.end('no such location')
+    res.json({
+			statusCode: 404,
+			message: 'Not Found'
+		});
   })
 }).listen(7777)
 
 handler.on('error', function (err) {
-  console.error('Error:', err.message)
+  console.error('Error: ', err.message)
 })
 
 handler.on('push', function (event) {
   if(event.payload.ref === process.env.GIT_REPOSITORY_REF){
-    console.log('pulling');
+    console.log("Execute Pull Command");
     simpleGit.pull(process.env.GIT_REPOSITORY_REMOTE, process.env.GIT_REPOSITORY_BRANCH, (response) => {
-      console.log("Pull success");
+      console.log("Pull Command Executed!");
     })
   }
-  console.log('Received a push event for %s to %s',
+  console.log('Received a push event from %s to %s',
     event.payload.repository.name,
     event.payload.ref)
 })
